1239945ffe445caabe8e291a59667d9f8cc86867
Isaiah Mari Quinicot <mixmycoffee@gmail.com>
2020-10-10 19:50:45 +0800

    login and logout system is working

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const mysql = require("mysql");
 
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
@@ -10,6 +12,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 app.use(urlEncodedParser);
+
+let isLoggedIn = 0; //Since we don't have sessions, we use this to temporarily tell if the user is logged in
+let UID = -1; //User id when logging in
+
 
 const connection = mysql.createConnection({
     host: "127.0.0.1",
@@ -26,27 +32,74 @@ connection.connect((err)=>{
 //Need to put na if get("/") niya wa ka login kay mo adto sa "/login", pero if naka login kay mo adto sa "/home"
 
 
+app.get("/", (req,res)=>{
+    if(isLoggedIn == 0){
+        res.redirect("/landing");
+    }else{
+        res.redirect("/home");
+    }
+})
+
 app.get("/home", (req,res)=>{
-    res.render('home', {title: "Home", navbarHeader: "Book-San", user: "Admin"});
+    if(isLoggedIn == 0){
+        res.redirect("/landing");
+    }else{
+        connection.query("SELECT username FROM account WHERE account_id = '" + UID + "'", (err, response)=>{
+            if (err) throw err;
+            let username = response[0]['username'];
+            res.render('home', {title: "Home", navbarHeader: "Book-San", user: username});
+        })
+    }
 })
 
 app.get("/account", (req,res)=>{
-    res.render('account', {title: "User Profile", navbarHeader: "User Profile"});
+    if(isLoggedIn == 0){
+        res.redirect("/landing");
+    }else{
+        connection.query("SELECT username FROM account WHERE account_id = '" + UID + "'", (err, response)=>{
+            if (err) throw err;
+            let username = response[0]['username'];
+            res.render('account', {title: "User Profile", navbarHeader: "User Profile", user: username});
+        })
+    }
+    
 })
 
 app.get("/add_product", (req,res)=>{
-    res.render('add_product', {title: "Book-san Inventory", navbarHeader:"Product Inventory"})
+    res.render('add_product', {title: "Book-san Inventory", navbarHeader:"Product Inventory"});
 })
 
-app.get("/landing", (req,res)=>{
-    res.render('landing', {title: "Book-san"})
+app.get(["/landing", "/landing/:status"], (req,res)=>{
+    if(req.params.status == "logout"){
+        UID = -1;
+        isLoggedIn = 0;
+    }
+    res.render('landing', {title: "Book-san"});
 })
 
 app.post("/login", (req,res)=>{
-//    let userPass = (using bcrypt)
-    connection.query('SELECT username, password, isAdmin FROM `account` WHERE `username` ="'+req.params.username+'" && `password` ="'+userPass+'"', (err,results)=>{
+    connection.query("SELECT * from account WHERE username = '"+req.body.username+"'", (err,response)=>{
         if(err) throw err;
-        res.redirect("/home");
+        console.log(response);
+        if(bcrypt.compareSync(req.body.password, response[0]['password'])){
+            UID = response[0]['account_id'];
+            isLoggedIn = 1;
+            res.send({redirect: "/home"});
+        //    res.send("true");
+        }else{
+        //    res.send("false");
+            res.send({redirect: "/landing?error=1"});
+        }
+    })
+})
+
+app.post("/create_user", (req,res)=>{
+    console.log(req.query);
+    let salt = bcrypt.genSaltSync(saltRounds);
+    let hash = bcrypt.hashSync(req.query.password, salt);
+    connection.query("INSERT INTO account (username, password, isAdmin) VALUES ('"+ req.query.username + "', '"+hash+"', '"+req.query.isAdmin+"')", (err, response)=>{
+        if (err) throw err;
+        res.send("Here");
     })
 })
 
--- public/assets/js/pages/login.js
+++ public/assets/js/pages/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('form').on('submit', function(){
+    $('#loginForm').on('submit', function(){
   
         var inputUser = $('#usernameInput').val();
         var inputPass = $('#passwordInput').val();
@@ -8,14 +8,21 @@ $(document).ready(function(){
         $.ajax({
           type: 'POST',
           url: '/login',
-          data: {username: inputUser, password: inputPass},
+          data: {
+            username: inputUser,
+            password: inputPass
+          },
           success: function(data){
             //do something with the data via front-end framework
-            location.reload();
+            if(data.redirect == "/landing?error=1"){
+              var showError = $('#errorText').css("display", "block");
+            }else{
+              window.location = data.redirect;
+            }
           }
         });
   
         return false;
   
     });
-})
+ })
